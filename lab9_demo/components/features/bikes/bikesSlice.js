31cbc4df184e09dd17a0e920647d87f214628259
tienminhtran <tientot36@gmail.com>
2024-11-11 13:24:42 +0700

    api cho bikes

--- lab9_demo/components/features/bikes/bikesSlice.js
+++ lab9_demo/components/features/bikes/bikesSlice.js
@@ -1,12 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchBikes = createAsyncThunk('bikes/fetchBikes', async () => {
-  const response = await fetch('https://6717c890b910c6a6e029f784.mockapi.io/bike');
+  const response = await fetch('https://67150c8133bc2bfe40b93603.mockapi.io/xedap');
   return response.json();
 });
 
 export const addBike = createAsyncThunk('bikes/addBike', async (newBike) => {
-  const response = await fetch('https://6717c890b910c6a6e029f784.mockapi.io/bike', {
+  const response = await fetch('https://67150c8133bc2bfe40b93603.mockapi.io/xedap', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
